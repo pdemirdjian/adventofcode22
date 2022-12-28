c847e3cd0591b22d40a81d6403bfc27d88deb0f8
Peter Demirdjian <code@peterdemirdjian.com>
2022-12-27 22:19:10 -0500

    this is day 1, damn

---
+++ util/util.go
@@ -0,0 +1,77 @@
+package util
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+const RootInputPath string = "/Users/peterdemirdjian/adventofcode22/inputs/"
+
+type Day struct {
+	Number    int
+	PartA     bool
+	InputPath string
+	TestPath  string
+	Input     string
+	Result    int
+	Passed    bool
+}
+
+type Problem interface {
+	Execute(*os.File) int
+	Test(*os.File) bool
+	Load() *os.File
+	LoadTest() *os.File
+	GetDay() int
+	GetPartA() bool
+}
+
+func fileLoad(path string) *os.File {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln("Couldn't open file at "+path, err)
+	}
+	return file
+}
+
+func (day Day) Load() *os.File {
+	if day.InputPath != "" {
+		return fileLoad(day.InputPath)
+	} else {
+		fmt.Println("Load:" + day.notImplemented())
+		return nil
+	}
+}
+
+func (day Day) LoadTest() *os.File {
+	if day.TestPath != "" {
+		return fileLoad(day.TestPath)
+	} else {
+		fmt.Println("LoadTest:" + day.notImplemented())
+		return nil
+	}
+}
+
+func (day Day) Execute(file *os.File) int {
+	fmt.Println("Execute:" + day.notImplemented())
+	return day.Result
+}
+
+func (day Day) Test(file *os.File) bool {
+	fmt.Println("Test:" + day.notImplemented())
+	return day.Passed
+}
+
+func (day Day) GetDay() int {
+	return day.Number
+}
+
+func (day Day) GetPartA() bool {
+	return day.PartA
+}
+
+func (day Day) notImplemented() string {
+	return " Day " + strconv.Itoa(day.Number) + " not implemented yet."
+}
